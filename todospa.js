8b09fbc927e944b8f1a4d0aede2eca316e384ce1
nelsje08 <nelsje08@luther.edu>
2015-10-16 13:15:21 -0500

    Update todospa.js

--- todospa.js
+++ todospa.js
@@ -1,2 +1,9 @@
 
 no script tag needed. just start writing code
+
+addlistitem = function() {
+     var myinput, myh2
+         myinput = document.querySelector('#newtask').value;
+     	   myh2 = document.querySelector('main');
+     	   myh2.innerHTML = myinput;
+   }
