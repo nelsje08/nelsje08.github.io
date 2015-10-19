e7c5aecba6f20a06ec59c897b14e91413a8b0474
nelsje08 <nelsje08@luther.edu>
2015-10-18 23:30:32 -0500

    Update todospa.js

--- todospa.js
+++ todospa.js
@@ -1,12 +1,16 @@
 
 addlistitem = function() {
-     var myinput, mymainlist, value1;
+     var myinput, mymainlist, value1, check;
          myinput = document.querySelector('#newtask');
          value1 = document.createTextNode(myinput.value);
          mymainlist = document.querySelector('#tasklist');
+        
          
-         item=document.createElement("li");
+         item=document.createElement('#checkbox');
          item.appendChild(value1);
-         mymainlist.appendChild(item);
+         mymainlist.appendChild(item)
+         item2=document.createElement("li");
+         item2.appendChild(value1);
+         mymainlist.appendChild(item2);
    }
 
