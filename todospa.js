c6d3b2430393ef4c290459a5e1fb154cb3f858a3
nelsje08 <nelsje08@luther.edu>
2015-10-19 12:23:30 -0500

    Update todospa.js

--- todospa.js
+++ todospa.js
@@ -5,16 +5,14 @@ addlistitem = function() {
          value1 = document.createTextNode(myinput.value);
          mymainlist = document.querySelector('#tasklist');
         // dropdowninput = document.querySelector("#priority");
-         
         // dropdowninput.value
         // need to create new querySelector to get dropdown input value and assign className to list item.... 
-        
         
      
          input=document.createElement("input");
          input.type="checkbox";
          item=document.createElement("li");
-         mymainlist.appendChild(input);
+         item.appendChild(input);
          item.appendChild(value1);
          mymainlist.appendChild(item);
    }
