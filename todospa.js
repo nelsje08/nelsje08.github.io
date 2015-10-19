ac10566fbcf7a75c517f009c6033ed9b6ebde410
nelsje08 <nelsje08@luther.edu>
2015-10-18 22:23:41 -0500

    Update todospa.js

--- todospa.js
+++ todospa.js
@@ -1,11 +1,10 @@
 
-no script tag needed. just start writing code
-
 addlistitem = function() {
-     var myinput, mymainlist
-         myinput = document.querySelector('#newtask').value;
+     var myinput, mymainlist, value1;
+         myinput = document.querySelector('#newtask');
+         value1 = textNode.querySelector(myinput.value);
          mymainlist = document.querySelector('#tasklist');
-         mymainlist.innerHTML = myinput;
+         mymainlist.innerHTML = value1;
    }
 
 var myinput, myh2
@@ -14,3 +13,14 @@ var myinput, myh2
      	   myh2.innerHTML = myinput;
            myh2.className = "spinanimation";
      	   
+add = function() {
+     var myIn1, myIn2, value1, value2, resultFunc
+           myIn1 = document.querySelector('#in1id');
+     	   value1 = Number.parseInt(myIn1.value);
+     	   myIn2 = document.querySelector('#in2id');
+     	   value2 = Number.parseInt(myIn2.value);
+     	   resultFunc = document.querySelector('#result');
+     	   calculatorFunc = (value1+value2);
+     	   resultFunc.innerHTML = calculatorFunc;
+  		  
+   }
