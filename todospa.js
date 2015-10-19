f9be6f0b9021816524553251ea37694fedd07d09
nelsje08 <nelsje08@luther.edu>
2015-10-18 22:40:14 -0500

    Update todospa.js

--- todospa.js
+++ todospa.js
@@ -7,20 +7,3 @@ addlistitem = function() {
          mymainlist.innerHTML = value1;
    }
 
-var myinput, myh2
-           myinput = document.querySelector('#newword').value;
-     	   myh2 = document.querySelector('#oldword');
-     	   myh2.innerHTML = myinput;
-           myh2.className = "spinanimation";
-     	   
-add = function() {
-     var myIn1, myIn2, value1, value2, resultFunc
-           myIn1 = document.querySelector('#in1id');
-     	   value1 = Number.parseInt(myIn1.value);
-     	   myIn2 = document.querySelector('#in2id');
-     	   value2 = Number.parseInt(myIn2.value);
-     	   resultFunc = document.querySelector('#result');
-     	   calculatorFunc = (value1+value2);
-     	   resultFunc.innerHTML = calculatorFunc;
-  		  
-   }
