5bdfa3628290a6f2539b5286a0bc09221f3baa79
nelsje08 <nelsje08@luther.edu>
2015-10-21 12:58:50 -0500

    Update todospa.js

--- todospa.js
+++ todospa.js
@@ -3,24 +3,24 @@ addlistitem = function() {
          myinput = document.querySelector('#newtask');
          value1 = document.createTextNode(myinput.value);
          mymainlist = document.querySelector('#tasklist');
-  
-         cb=document.createElement("INPUT");
-                  alert = "is this working";
-         cb.type="checkbox";
-                  alert = "is this working";
-         cb.onclick = checkfunc;
-                  alert = "is this working";
-         myLi=document.createElement("li");
-                  alert = "is this working";
-         myLi.appendChild(cb);
-                  alert = "is this working";
-         myLi.appendChild(value1);
-                  alert = "is this working";
-         mymainlist.appendChild(myLi);
-                  alert = "is this working";
-                  
+         
          dropdowninput = document.querySelector("#priority");
          myLi.classList.add(dropdowninput.value);
+  
+         cb=document.createElement("INPUT");
+
+         cb.type="checkbox";
+                 
+         cb.onclick = checkfunc;
+                  
+         myLi=document.createElement("li");
+                  
+         myLi.appendChild(cb);
+                 
+         myLi.appendChild(value1);
+                 
+         mymainlist.appendChild(myLi);
+                
 }
 
 checkfunc = function() {
